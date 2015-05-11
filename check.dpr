cbb04ec280e518978b1a85c2d9339f6533594f99
Mark Tertishniy <mtertishniy@gmail.com>
2015-05-11 20:12:15 +1000

    Fix code style: add line ending before 'begin' statement

--- check.dpr
+++ check.dpr
@@ -10,10 +10,13 @@ function parseDouble(s: string): double;
 var
     code: integer;
 begin
-    if s <> '*' then begin
+    if s <> '*' then
+    begin
         val(s, result, code);
         if code <> 0 then quit(_pe, format('Expected real instead of "%s"', [s]));
-    end else begin
+    end
+    else
+    begin
         parseDouble := inf;
     end;
 end;
@@ -31,16 +34,19 @@ begin
     ja := ans.readLongint();
     pa := ouf.readLongint();
 
-    if ja <> pa then begin
+    if ja <> pa then
+    begin
         quit(_wa, format('%d segments instead of %d', [pa, ja]));
     end;
 
-    for i := 1 to ja do begin
+    for i := 1 to ja do
+    begin
         jl := parseDouble(ans.readWord(blanks, blanks));
         jr := parseDouble(ans.readWord(blanks, blanks));
         pl := parseDouble(ouf.readWord(blanks, blanks));
         pr := parseDouble(ouf.readWord(blanks, blanks));
-        if (not eq(jl, pl) or not eq(jr, pr)) then begin
+        if (not eq(jl, pl) or not eq(jr, pr)) then
+        begin
             quit(_wa, format('Segment %d: (%.10f, %.10f) instead of (%.10f, %.10f)', [i, pl, pr, jl, jr]));
         end;
     end;
